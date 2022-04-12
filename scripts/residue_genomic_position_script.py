714da13133ab39e5bf96fab51ebc1a49f5c099ba
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2022-04-12 18:11:14 +0100

    Fully re-enable Pandarallel

--- scripts/residue_genomic_position_script.py
+++ scripts/residue_genomic_position_script.py
@@ -103,7 +103,7 @@ def main():
 
     # Pandas Apply
     plip_output_agg_pd = plip_output_agg.toPandas()
-    plip_output_agg_pd['res_infos'] = plip_output_agg_pd.apply(
+    plip_output_agg_pd['res_infos'] = plip_output_agg_pd.parallel_apply(
         fetch_gapi_ensembl_mapping, axis=1
     )
 
